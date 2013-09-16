f7efb036c3fac271b5a28d1e8b319e82c469e9da
olhasquew <olhasquew@olhasquew-PC.wopint>
2013-09-16 20:42:55 +0200

    Initial commit
    Netbeans C++ QT Project

---
+++ TestOffFillReader/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: olhasquew
+ *
+ * Created on 16 septembre 2013, 20:42
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
